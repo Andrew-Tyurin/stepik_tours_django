6c259f208f60adc898fb2cf0799c2e3c8cd874de
Andrew-Tyurin <anrej.andrej2017@ya.ru>
2025-08-27 16:57:49 +0300

    Оптимизация Urls для tours
    Маршруты для приложения tours были перенесены из корневой директории проекта в свою директорию приложения.

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path
-import tours.views as tours_views
+from django.urls import path, include
 from tours.views import custom_handler404, custom_handler500
 
 handler500 = custom_handler500
@@ -8,7 +7,5 @@ handler404 = custom_handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', tours_views.main_view, name='main_view'),
-    path('departure/<str:departure>', tours_views.departure_view, name='departure_view'),
-    path('tour/<int:tour_id>', tours_views.tour_view, name='tour_view'),
+    path('', include('tours.urls')),
 ]
---
+++ tours/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main_view, name='main_view'),
+    path('departure/<str:departure>', views.departure_view, name='departure_view'),
+    path('tour/<int:tour_id>', views.tour_view, name='tour_view'),
+]
