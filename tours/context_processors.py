4724184f27d701e03bae451b74de9ec33b4fc3c1
Andrew-Tyurin <anrej.andrej2017@ya.ru>
2025-08-29 01:11:01 +0300

    Реализовал Context processor
    Данный скрипт нужен для вывода информации в навигационную панель, которая не зависит от запроса.

---
+++ tours/context_processors.py
@@ -0,0 +1,13 @@
+from . import data
+
+def global_data(request):
+    """
+    Context processor — это функция, которая автоматически
+    добавляет данные во все шаблоны. Реализация в views.py
+    можно, но но это считается плохим стилем, т.к. view
+    и процессоры — разные вещи. Эта функция нужна для вывода
+    данных в навигационную панель которая не завесит от запроса.
+    Не забываем в setting.py -> TEMPLATES дополнить:
+    'tours.context_processors.global_data'
+    """
+    return {'departures': data.departures,}
